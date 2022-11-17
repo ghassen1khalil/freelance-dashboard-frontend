f7995fd1da3525ab7ce760ef9d312ca44f484959
Ghassen <ghassen1khalil@gmail.com>
2022-11-18 00:27:35 +0100

    fix(startingDate): configure the utc for startingDate #16

---
+++ freelance-dashboard/src/services/date.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateService {
+
+  static readonly YYYY_MM_DD_FORMAT: string = "YYYY-MM-DD";
+
+  constructor() { }
+
+  public today(format: string) {
+    return moment().format(format);
+  }
+
+  public formatAndUtc(date: string ,format: string) {
+    return moment(date).utc(true).format(format);
+  }
+}
