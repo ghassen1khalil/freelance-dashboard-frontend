c5da924ec46542f63ae35a6202c3c2b6dc2f3713
Ghassen <ghassen1khalil@gmail.com>
2023-05-22 18:04:48 +0200

    feat: impelement notification after LaunchEvent action #23

--- freelance-dashboard/src/app/app.component.ts
+++ freelance-dashboard/src/app/app.component.ts
@@ -1,20 +1,18 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-/*import {MessageService, PrimeNGConfig} from 'primeng/api';*/
 import {select, Store} from '@ngrx/store';
 import {Event} from '../core/store/models/models';
 import {Subject, takeUntil} from 'rxjs';
 import * as eventReducer from '../core/store/reducers/event.reducer'
-import * as PositionActions from '../core/store/actions/position.actions';
 import {TranslateService} from '@ngx-translate/core';
-import {PrimeNGConfig} from 'primeng/api';
+import {MessageService, PrimeNGConfig} from 'primeng/api';
 import {AuthService} from '@auth0/auth0-angular';
 import {getAuth} from '../core/store/reducers/auth.reducers';
-import {isNotNullOrUndefined} from 'codelyzer/util/isNotNullOrUndefined';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService]
 })
 export class AppComponent implements OnInit, OnDestroy {
 
@@ -25,27 +23,21 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor(private store: Store<{ event: Event }>,
               private translateService: TranslateService,
               private config: PrimeNGConfig,
-              private authService: AuthService
-              /*private messageService: MessageService,*/
-              /*private primengConfig: PrimeNGConfig*/) {
+              private authService: AuthService,
+              private messageService: MessageService,
+              private primengConfig: PrimeNGConfig) {
     this.translateService.use('fr');
     this.translateService.get('primeng').subscribe(res => this.config.setTranslation(res));
-
-    /*this.authService.isAuthenticated$.subscribe(isAuthenticated => {
-      this.isHeaderShown = isAuthenticated && (!window.location.href.includes('login') || !window.location.href.includes('signup'));
-    });*/
   }
 
   ngOnInit() {
-    //this.primengConfig.ripple = true;
-    this.store.pipe(
-      select(eventReducer.getEvent),
-      takeUntil(this.unsubscribe$)
-    ).subscribe(event => {
-      //console.log(event?.body);
-      //this.messageService.add({severity:'success', summary: event?.title, detail: event?.body});
-    });
+    this.primengConfig.ripple = true;
+    this.checkIfHeaderIsShown();
+    this.listenToNotification();
 
+  }
+
+  private checkIfHeaderIsShown(): void {
     this.store.pipe(
       select(getAuth),
       takeUntil(this.unsubscribe$)
@@ -54,17 +46,15 @@ export class AppComponent implements OnInit, OnDestroy {
     });
   }
 
-  /*onConfirm() {
-    this.messageService.clear('c');
-  }*/
-
-  /*onReject() {
-    this.messageService.clear('c');
-  }*/
-
-  /*clear() {
-    this.messageService.clear();
-  }*/
+  private listenToNotification(): void {
+    this.store.pipe(
+      select(eventReducer.getEvent),
+      takeUntil(this.unsubscribe$)
+    ).subscribe(event => {
+      console.log(event?.body);
+      this.messageService.add({severity: event?.type, summary: event?.title, detail: event?.body});
+    });
+  }
 
   ngOnDestroy(): void {
     this.unsubscribe$.complete();
